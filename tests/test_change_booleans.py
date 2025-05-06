b06f71963daee574f0399484eccac388e5cf5c57
Artyom Rozhkov <argue364@gmail.com>
2025-05-06 20:40:37 +0200

    add new test

---
+++ tests/test_change_booleans.py
@@ -0,0 +1,16 @@
+from gendiff.scripts.gendiff import change_booleans
+
+TEST_DICT = {
+    "timeout": 20,
+    "verbose": True,
+    "host": "hexlet.io"
+}
+
+RES_DICT = {
+    "timeout": 20,
+    "verbose": 'true',
+    "host": "hexlet.io"
+}
+
+def test_change_booleans():
+    assert change_booleans(TEST_DICT) == RES_DICT
